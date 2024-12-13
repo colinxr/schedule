948a49f955f3db939a85db5c1c82c2447e42476e
Colin Rabyniuk <colinxr@gmail.com>
2024-12-13 12:05:33 -0800

    better handle redirect to dashboard

--- web/app/auth/layout.tsx
+++ web/app/auth/layout.tsx
@@ -14,25 +14,18 @@ export default function AuthLayout({
   const { isAuthenticated, loading } = useAuth();
 
   useEffect(() => {
-    console.log('isAuthenticated', isAuthenticated);
-    console.log('loading', loading);
-    
-    // if (!loading && isAuthenticated) {
-    //   router.push('/a/conversations');
-    // }
+    if (!loading && isAuthenticated) {
+      router.push('/a/conversations');
+    }
   }, [isAuthenticated, loading, router]);
 
-//   if (loading) {
-//     return (
-//       <div className="flex items-center justify-center h-screen">
-//         <Loader2 className="h-4 w-4 animate-spin" />
-//       </div>
-//     );
-//   }
-
-//   if (isAuthenticated) {
-//     return null;
-//   }
+  if (loading || isAuthenticated) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader2 className="h-4 w-4 animate-spin" />
+      </div>
+    );
+  }
 
   return children;
 } 
